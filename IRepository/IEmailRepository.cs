080c713d2079534635d435e17130f8403c4ee296
Himanshu Upreti <105162770+Himanshu1117@users.noreply.github.com>
2024-08-02 07:41:37 +0530

    Add files via upload
    IReposotory

---
+++ IRepository/IEmailRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Model.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace IRepository
+{
+    public  interface IEmailRepository
+    {
+        //1.Method to Send the email to user when it clicks on forgetPassword.
+        public Task<OperationResult> SendResetEmailRepo(string email);
+
+
+        //2.Method to Reset the Password
+        public Task<OperationResult> ResetPassword(ResetPasswordDto resetPassword);
+    }
+}
---
+++ IRepository/IUserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Data.Model.Dto;
+using Data.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace IRepository
+{
+    public interface IUserRepository
+    {
+
+        public Task<(IEnumerable<User> Users, int TotalUsers)> GetUsers(int pageNumber, int pageSize);
+
+        //method to edit the user
+        public Task<bool> EditUsers(string id, UserDetailsDto userDetails, bool status);
+
+        //method to delete the user
+        public Task<bool> DeleteUsers(int id);
+
+        //method to filter the user
+        public Task<IEnumerable<User>> FilterUsers(string status);
+
+        //  method to sort the user
+        public Task<IEnumerable<User>> SortUsers(String sort_order);
+
+        //method to post the user image
+        // public Task<string> AddUsers(IFormFile image);
+
+        //method to create the user 
+
+        // public Task<string> CreateUsers(UserDto user, string path);
+
+        public Task<string> CreateUsers(UserDetailsDto user);
+
+        public Task<User> GetUserById(int id);
+        // method to change password
+        public Task<bool> ChangePassword(ChangePasswordDto changePassword);
+
+
+        //method to export excel
+        public Task<IActionResult> ExportToExcel();
+
+    }
+}
